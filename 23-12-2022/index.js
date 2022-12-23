6290f35aedeca0bf65513b2a9feabee6a96aff39
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2022-12-23 12:33:19 +0500

    23-12-2022

---
+++ 23-12-2022/index.js
@@ -0,0 +1,41 @@
+// let  items = ["cake", "apple pie", "cookie", "chips", "patties"] //chips
+
+// let choice=prompt("Enter what you want to buy?")
+
+// for(let i=0;i<items.length;i++){
+//     if(items[i]===choice.toLowerCase()){
+//         document.write("It is available")
+//     }
+// }
+// let  items = ["cake", "apple pie", "cookie", "chips", "patties"] //chips
+
+// let choice=prompt("Enter what you want to buy?")
+// function myFunction(items){
+
+
+// for(let i=0;i<items.length;i++){
+//     if(items[i]===choice.toLowerCase()){
+//         document.write("It is available")
+//     }
+// }
+
+// }
+
+// myFunction(items)
+
+
+
+let userInput=prompt("enter any string")
+let userInput1=prompt("enter any string")
+
+function task(user){
+let result=user.toUpperCase()
+return result
+
+}
+
+let output=task(userInput)
+document.write("The output from function is: "+output)
+document.write("<br>")
+let output1=task(userInput1)
+document.write("The output from function is: "+output1)
